2de558a718935186ab5be59893de96e3d6c4e7cb
Massilia23 <massiliabakhouche@gmail.com>
2025-06-13 15:43:18 +0200

    jointure inner aboutie

--- notebooks/OLD/test_spark.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.master("local[*]").appName("TestSpark").getOrCreate()
-
-df = spark.createDataFrame([(1, "a"), (2, "b")], ["id", "value"])
-df.show()
-
-spark.stop()
